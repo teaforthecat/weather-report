3a2f25fc141b6e5c3dbb770eb1086bf158a898d6
Chris Thompson <teaforthecat@gmail.com>
2016-08-04 22:14:28 -0500

    [cljs] login handler

--- src/cljs/weather_report/handlers.cljs
+++ src/cljs/weather_report/handlers.cljs
@@ -17,17 +17,12 @@
 (defmulti submit-form (fn [form-id _ _ _] form-id))
 
 (defmethod submit-form :login [form-id db form default-form]
-  (let [form-data (select-keys @form [:username :password])
-        response  (a/take! (client/post "http://localhost:8080/api/login"
-                                        {:command :login
-                                         :args form-data})
-                           println) ]
-   ;; move to handler
-    (if (= 200 (:status response))
-      (do
-        (reset! form default-form)
-        (assoc db :bones/logged-in? true))
-      (println response))))
+  (let [form-data (select-keys @form [:username :password])]
+    (a/take! (client/post "http://localhost:8080/api/login"
+                          {:command :login
+                           :args form-data})
+             #(re-frame/dispatch [:login-handler % form default-form]))
+    db))
 
 (defmethod submit-form :add-account [form-id db form default-form]
   (let [data @form]
@@ -43,3 +38,14 @@
  :logout
  (fn [db [_ form-ratom]]
    (assoc db :bones/logged-in? false)))
+
+(re-frame/register-handler
+ :login-handler
+ (fn [db [_ response form default-form]]
+   (if (= 200 (:status response))
+     (do
+       (reset! form default-form)
+       (assoc db :bones/logged-in? true))
+     (do
+       (println response)
+       db))))
