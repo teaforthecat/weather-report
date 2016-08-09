c1645a46dc2b916cb4269483aaf7c109c7fc7082
Chris Thompson <teaforthecat@gmail.com>
2016-08-08 23:37:37 -0500

    update -main bones init

--- src/clj/weather_report/core.clj
+++ src/clj/weather_report/core.clj
@@ -2,11 +2,11 @@
   (:gen-class)
   (require [com.stuartsierra.component :as component]
            [schema.core :as s]
-           [bones.http.service :as service]
-           [bones.http.handlers :as handlers]
-           [bones.conf :as conf]))
+           [bones.http.core :as http]
+           [bones.http.handlers :as handlers]))
 
 (def sys (atom {}))
+
 (defn login [args req]
   {:user-id 123 :roles ["tester"]})
 
@@ -19,26 +19,21 @@
 (handlers/register-command :add-account {:account/xact-id s/Int
                                          :account/evo-id (s/maybe s/Int)})
 
-(def conf {:http/auth {:secret "a 16 byte stringa 32 byte string"}})
+(def conf {:http/auth {:secret "a 16 byte stringa 32 byte string"}
+           :http/handlers {:mount-path "/api"}})
 
-
-(defn init-system []
-  ;; note: this could also be a Conf component
-  (swap! sys assoc :conf {:http/handlers {:mount-path "/api"}})
-  (swap! sys assoc :routes (component/using
-                            (handlers/map->CQRS {})
-                            [:conf]))
-  (swap! sys assoc :http (component/using
-                          (service/map->Server {})
-                          [:conf :routes])))
+(defn init-system [config]
+  (http/build-system sys config))
 
 (defn -main
   "The entry-point for 'lein run'"
   [& args]
-  (init-system)
-  (swap! sys assoc-in [:conf :http/service :join?] true) ;; this will block the caller forever
-  (service/start-system sys :http :routes :conf))
+  ;; join? true will block the caller forever
+  (init-system (assoc-in conf [:http/service :join?] true))
+  (http/start-system sys))
 
 (comment
-  (service/stop-system sys :http)
+ (init-system config)
+ (http/start-system sys)
+  (http/stop-system sys)
   )
