8791f9ced7b521f0a3bfc83fc8905e6461beddbc
Christopher Thompson <cthompson@megatron.local>
2018-07-08 21:30:11 -0500

    neat test to markdown tricks

--- test/browser/weather_report/login_test.clj
+++ test/browser/weather_report/login_test.clj
@@ -2,14 +2,28 @@
   (:require [clojure.test :refer [deftest testing is]]
             [etaoin.api :as e]
             [etaoin.keys :as k]
-            [clojure.spec :as s]
-            [clojure.java.io :as io]))
+            [clojure.spec.alpha :as s]
+            [clojure.java.io :as io]
+            [manifold.deferred :as d]))
+
+
 
 (def conf (atom {:driver :phantom
-                 ;; :site "http://localhost:3449/"
-                 :site (str "file://" (.getPath (io/resource "public/index.html")))
+                 :site "http://localhost:8080/"}))
 
-                 }))
+(def level-1 "# %s \n")
+(def level-2 "## %s \n")
+(def level-3 "%s \n\n")
+(def level-4 "%s \n\n")
+
+(defmethod clojure.test/report :pass [m]
+  (let [rep (clojure.test/inc-report-counter :pass)]
+    (clojure.test/with-test-out
+      (printf level-3 (str (:message m) " (ok)")))))
+
+(defmethod clojure.test/report :begin-test-var [m]
+  (clojure.test/with-test-out
+    (printf level-2 (-> m :var meta :name))))
 
 
 (defn login [d username password]
@@ -20,20 +34,27 @@
     (e/click :submit)))
 
 (defmacro then [text assertion]
-  `(is ~assertion ~text))
+  `(is ~assertion (str "then " ~text)))
 
 (defmacro given [text letform & body]
   `(testing ~text
+     (clojure.test/with-test-out
+       (printf level-3 (str "Given " ~text)))
+     ;; need to print before body for proper nesting
     (let ~letform
       ~@body)))
 
 (defmacro pose [text & body]
-  `(let [t# ~text]
-     ~@body))
+  `(let [t# ~text
+         result# (do ~@body)]
+     ;; might be best to print after execution to indicate success
+     (clojure.test/with-test-out
+       (printf level-3 (str ~text)))))
 
 (defmacro note [& texts]
   `(let [t# (str ~@texts)]
-     ))
+     (clojure.test/with-test-out
+       (printf level-4 t#))))
 
 (defn text [label]
   (str"//*[text()='" label "']"))
@@ -41,23 +62,24 @@
 (deftest login-test
   (let [d (e/phantom)
         {:keys [site]} @conf
-        ;; site "http://localhost:3449/"
         username "abcd"
         password "abcd"]
-    (e/go d site)
-    (login d username password)
-    (then "the user is greeted with 'Hello'"
-          (e/has-text? d (str "Hello " username)))))
+    (testing "simple login"
+      (e/go d site)
+      (given "a user logs in with a valid username and password"
+             [_ (login d username password)]
+             (then "the user is greeted with 'Hello'"
+                   (e/has-text? d (str "Hello " username)))))))
 
 (defn new-account-fusion [d xact-id evo-id]
-  (pose "a user clicks 'New Account Fusion'"
+  (pose "and they click 'New Account Fusion'"
           (e/click d :new-account-fusion))
-  (pose "submits xact-id: 123 and evo-id: 321"
+  (pose "and they submit xact-id: 123 and evo-id: 321"
         (e/fill d :xact-id xact-id)
         (e/fill d :evo-id evo-id)
         (e/click d :submit)
         (e/wait-exists d :evo-id-123))
-  (then "should see 123"
+  (then "they should see 123"
         (e/has-text? d evo-id)))
 
 (defn update-value [d id value]
@@ -76,24 +98,23 @@
 (deftest add-account
   (given "a user is logged in"
          [d (e/phantom)
-          ;; site "http://localhost:3449/"
           {:keys [site]} @conf
           _  (e/go d site)
           _ (login d "abcd" "abcd")]
 
-         (pose "create a new account"
+         (pose "when they create a new account fusion"
                (new-account-fusion d "123" "321"))
 
-         (then "should see new value '321'"
+         (then "they should see new value '321'"
                (e/has-text? d "321"))
 
-         (pose "upate the evo-id to 456"
+         (pose "and when they update the evo-id to 456"
                (update-value d "123" "456"))
 
          (note "hitting enter while entering a value will submit the form,"
                "though it is not documented, it is assumed that this is understood to be normal browser behavior")
 
-         (then "should see new value '456'"
+         (then "they should see new value '456'"
                (e/has-text? d "456"))))
 
 (comment
@@ -107,8 +128,7 @@
 
   (new-account-fusion d "123" "321")
 
-  (new-account-fusion "456" "654")
-
+  (new-account-fusion d "456" "654")
 
   ;; (e/query d (text "321"))
 
@@ -120,6 +140,10 @@
   (e/fill d :evo-id
           k/escape)
 
+
+  (take 1
+   (reverse
+    (e/get-logs d)))
 
   (e/js-execute d "document.getElementById(arguments[0]).value = arguments[1];", "evo-id", "")
 
